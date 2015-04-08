e63d529ed923628ac267b5637626ed39b489c13a
Ryan Haase <ryandhaase@gmail.com>
2015-04-08 13:59:56 -0400

    Fixed application layout along with addign signup form to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,26 @@
 module ApplicationHelper
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
+  
   def my_name
     "Ryan Haase"
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
