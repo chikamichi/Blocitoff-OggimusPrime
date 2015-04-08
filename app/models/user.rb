432a4449a5d027a43cb91be1901e155e36857785
Ryan Haase <ryandhaase@gmail.com>
2015-04-07 20:31:09 -0400

    Updated all devise views with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
+
+  # Monkey patching: allows user to login via username or email address.
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
